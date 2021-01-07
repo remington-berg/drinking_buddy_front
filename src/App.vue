8b66bfc87a4978c2fd9da456130fc311d7080aa5
Remington <remingtonnberg@gmail.com>
2021-01-07 16:03:14 -0600

    adds filter buttons

--- src/App.vue
+++ src/App.vue
@@ -1,77 +1,123 @@
 <template>
   <div id="app">
-    <nav class="navbar navbar-expand-lg navbar-light bg-light">
-  <a class="navbar-brand" href="#">Navbar</a>
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
-  </button>
-
-  <div class="collapse navbar-collapse" id="navbarSupportedContent">
-    <ul class="navbar-nav mr-auto">
-      <li class="nav-item active">
-        <a class="nav-link" href="/">Home <span class="sr-only">(current)</span></a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="/restaurants">Restaurants</a>
-      </li>
-      <div v-if="isLoggedIn()">
-      <li class="nav-item">
-        <a class="nav-link" href="/restaurants/new">New Restaurants</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="/specials/new">New Special</a>
-      </li>
-      </div>
-      <li class="nav-item dropdown">
-        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-          User
-        </a>
-        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-          <div v-if="!isLoggedIn()">
-            <a class="dropdown-item" href="/signup">Sign Up</a>
-            <a class="dropdown-item" href="/login">Log In</a>
-          </div>
-          <div class="dropdown-divider"></div>
-            <div v-if="isLoggedIn()">
-              <a class="dropdown-item" href="/logout">Logout</a>
-            </div>
+      <div id="preloader">
+        <div class="preloader">
+            <span></span>
+            <span></span>
         </div>
-      </li>
-      <!-- <li class="nav-item">
-        <a class="nav-link disabled" href="#">Disabled</a>
-      </li> -->
-    </ul>
-    <!-- <form class="form-inline my-2 my-lg-0">
-      <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
-      <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button> -->
-    <!-- </form> -->
-  </div>
-</nav>
+    </div>
+
+    <div class="top-menu top-menu-primary">
+        <div class="container">
+            <p>
+                <span class="social margin-fix left">
+                    <a class="no-margin" href="https://www.facebook.com/remington.berg"><i class="fab fa-facebook-f"></i></a>
+                    <!-- <a href="#"><i class="fab fa-twitter"></i></a> -->
+                    <!-- <a href="#"><i class="fab fa-google-plus-g"></i></a> -->
+                    <a href="https://www.instagram.com/remington_b/?hl=en"><i class="fab fa-instagram"></i></a>
+                    <!-- <a href="#"><i class="fab fa-pinterest"></i></a> -->
+                    Get To Know Me
+                </span>
+                <span class="right">
+                    <a v-if='!isLoggedIn()' href="/login" ><i class="fas fa-user mr-1"></i> <span>Login</span></a>
+                    <a v-if='!isLoggedIn()' href="/signup" ><i class="fas fa-lock mr-1"></i> <span>Register</span></a>
+                    <a v-if='isLoggedIn()' href="/logout" ><i class="fas fa-user mr-1"></i> <span>Logout</span></a>
+                </span>
+            </p>
+        </div><!-- / container -->
+    </div><!-- / top-menu -->
+
+    <nav class="navbar navbar-expand-lg navbar-light bg-white custom-menu split-menu">
+        <div class="container">
+            <button class="navbar-toggler collapsed" type="button" data-toggle="collapse" data-target="#navbar-toggle-1" aria-controls="navbar-toggle-1" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="icon-bar top-bar"></span>
+                <span class="icon-bar middle-bar"></span>
+                <span class="icon-bar bottom-bar"></span>
+                <span class="sr-only">Toggle navigation</span>
+            </button><!-- / navbar-toggler -->
+
+            <a class="navbar-brand mobile-brand m-auto" href="/"><img src="images/logo-icon.png" alt=""></a>
+
+            <button class="navbar-toggler collapsed" type="button" data-toggle="collapse" data-target="#navbar-toggle-2" aria-controls="navbar-toggle-2" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="icon-bar top-bar"></span>
+                <span class="icon-bar middle-bar"></span>
+                <span class="icon-bar bottom-bar"></span>
+                <span class="sr-only">Toggle navigation</span>
+            </button><!-- / navbar-toggler -->
+
+            <div class="collapse navbar-collapse" id="navbar-toggle-1">
+                <ul class="navbar-nav mr-auto">
+                    <li class="nav-item after-dropdown">
+                        <a class="nav-link" href="/">Home</a>
+                    </li>
+                    <li class="nav-item after-dropdown">
+                        <a class="nav-link" href="/restaurants">Restaurants</a>
+                    </li>
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#x" id="dropdown2" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Categories</a>
+                        <div class="dropdown-menu drop-to-right animated fadeIn fast" aria-labelledby="dropdown2">
+                            <a class="dropdown-item" href="#x">Framed</a>
+                            <a class="dropdown-item" href="#x">Print</a>
+                            <a class="dropdown-item" href="#x">Digital</a>
+                            <a class="dropdown-item" href="#x">Photography</a>
+                        </div><!-- / dropdown-menu -->
+                    </li><!-- / dropdown -->
+                </ul><!-- / navbar-nav -->
+            </div><!-- / navbar-collapse -->
+
+            <a class="navbar-brand m-auto" href="/"><img src="images/logo-icon.png" alt=""></a>
+
+            <div class="collapse navbar-collapse" id="navbar-toggle-2">
+                <ul class="navbar-nav ml-auto">
+                    <li class="nav-item">
+                        <a v-if='isLoggedIn()' class="nav-link" href="/restaurants/new">New Restaurant</a>
+                    </li>
+                    <li class="nav-item">
+                        <a v-if='isLoggedIn()' class="nav-link before-count" href="/specials/new">New Special</a>
+                    </li>
+                    <li class="nav-item dropdown extra-dropdowns">
+                        <a class="nav-link last-menu-item has-dropdown-toggle dropdown-toggle" href="#x" id="dropdown3" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Shopping Cart<span class="count count-primary">2</span></a>
+                        <div class="dropdown-menu animated fadeIn fast" aria-labelledby="dropdown3">
+                            <div class="cart-small">
+                                <img src="images/product-small1.jpg" alt="">
+                                <p><a href="#x" class="text-black">Amazing Framed Art</a> <br> <span>1 x $29.99</span></p>
+                                <a href="#x"> <i class="md-icon dp14 close-icon">close</i></a>
+                            </div><!-- / cart-small -->
+                            <div class="cart-small">
+                                <img src="images/product-small2.jpg" alt="">
+                                <p><a href="#x" class="text-black">Printed Photography</a> <br> <span>1 x $14.99</span></p>
+                                <a href="#x"> <i class="md-icon dp14 close-icon">close</i></a>
+                            </div><!-- / cart-small -->
+                            <p class="text-left cart-small-total"><b>Subtotal: $44.98</b></p>
+                            <div class="cart-small-footer text-center">
+                                <div class="row">
+                                    <div class="col-sm-6">
+                                        <a href="shopping-cart.html" class="mini-cart-btn"><i class="md-icon dp12 mr-1">shopping_cart</i> <span class="va-middle"><b>VIEW CART</b></span></a>
+                                    </div><!-- / column -->
+                                    <div class="col-sm-6">
+                                        <a href="checkout.html" class="mini-cart-btn mb-0"><i class="md-icon dp12 mr-1">exit_to_app</i> <span class="va-middle"><b>CHECKOUT</b></span></a>
+                                    </div><!-- / column -->
+                                </div><!-- / row -->
+                            </div><!-- / cart-small-footer -->
+                        </div><!-- / dropdown-menu -->
+                    </li><!-- / dropdown -->
+                </ul><!-- / navbar-nav -->
+            </div><!-- / navbar-collapse -->
+        </div><!-- / container -->
+    </nav><!-- / split-navbar -->
+
+ 
+    
     <router-view/>
+    <footer class="bg-white">
+        <div class="container-fluid text-center">
+            <p>© 2018 <b>Gallerio</b> by <a href="https://kingstudio.ro" target="_blank">KingStudio</a>. All Rights Reserved.</p>
+        </div><!-- / container-fluid -->
+    </footer>
   </div>
 </template>
 
 <style>
-/* #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-} */
 </style>
 
 <script>
--- src/views/SpecialsIndex.vue
+++ src/views/SpecialsIndex.vue
@@ -1,8 +1,200 @@
 <template>
   <div>
+<div class="container">
     <h1>Drinking Buddy</h1>
-    <p>So tell me, when we drinking?</p>
+        <header class="home-header parallax">
+            <div class="header-content dark text-center">
+                <h1 class="header-title mb-0">Drinking Buddy</h1>
+                <p class="inner-space mb-0">Cheers!</p>
+            </div><!-- / header-content -->
+        </header>
+    </div><!-- / container -->
 
+            <section id="gallery" class="p-0 line-effect">
+        <div class="container">
+        <h2>So tell me, when we drinking?</h2>
+            <!-- gallery filter -->
+            <ul class="gallery-filter list-inline text-center">
+                <li><a href="#" data-group="all" class="active ml-2">SEE ALL</a></li>
+                <li><a href="#" @click='dayFilter="Monday"'>Monday</a></li>
+                <li><a href="#" @click='dayFilter="Tuesday"'>Tuesday</a></li>
+                <li><a href="#" @click='dayFilter="Wednesday"'>Wednesday</a></li>
+                <li><a href="#" @click='dayFilter="Thursday"'>Thursday</a></li>
+                <li><a href="#" @click='dayFilter="Friday"'>Friday</a></li>
+                <li><a href="#" @click='dayFilter="Saturday"'>Saturday</a></li>
+                <li><a href="#" @click='dayFilter="Sunday"'>Sunday</a></li>
+            </ul>
+            <!-- / gallery filter -->
+        </div><!-- / container -->
+        <div class="container full-width">
+            <h3 class="section-title hidden">GALLERY</h3>
+            <ul class="row gallery line-effect list-unstyled mb-0" id="grid">
+                <!-- gallery -->
+                <li class="col-md-6 col-lg-4 gallery" data-groups='["framed"]'>
+                    <figure class="gallery-item effect-bubba">
+                        <img src="images/gallery1.jpg" alt="">
+                        <figcaption>
+                            <div class="hover-content">
+                                <h2 class="hover-title text-center text-white">AMAZING FRAMED ART</h2><!-- / hover-title -->
+                                <p class="gallery-info text-center text-white">Framed - From $29.99
+                                    <span class="gallery-icons">
+                                        <a href="#x" class="gallery-button" data-toggle="modal" data-target=".framed-product"><i class="fas fa-plus"></i></a>
+                                        <a href="shopping-cart.html" class="gallery-button"><i class="fas fa-shopping-cart"></i></a>
+                                    </span><!--/ gallery-icons -->
+                                </p><!-- / gallery-info -->
+                            </div><!-- / hover-content -->
+                        </figcaption>
+                    </figure><!-- / gallery-item -->
+                </li><!-- / gallery -->
+
+                <!-- gallery -->
+                <li class="col-md-6 col-lg-4 gallery tall-gallery" data-groups='["photography", "print"]'>
+                    <figure class="gallery-item effect-bubba">
+                        <img src="images/mgallery2.jpg" alt="">
+                        <figcaption>
+                            <div class="hover-content">
+                                <h2 class="hover-title text-center text-white">PRINTED PHOTOGRAPHY</h2><!-- / hover-title -->
+                                <p class="gallery-info text-center text-white">Print - From $14.99
+                                    <span class="gallery-icons">
+                                        <a href="#x" class="gallery-button" data-toggle="modal" data-target=".print-product"><i class="fas fa-plus"></i></a>
+                                        <a href="shopping-cart.html" class="gallery-button"><i class="fas fa-shopping-cart"></i></a>
+                                    </span><!--/ gallery-icons -->
+                                </p><!-- / gallery-info -->
+                            </div><!-- / hover-content -->
+                        </figcaption>
+                    </figure><!-- / gallery-item -->
+                </li><!-- / gallery -->
+
+                <!-- gallery -->
+                <li class="col-md-6 col-lg-4 gallery" data-groups='["framed"]'>
+                    <figure class="gallery-item effect-bubba">
+                        <img src="images/gallery3.jpg" alt="">
+                        <figcaption>
+                            <div class="hover-content">
+                                <h2 class="hover-title text-center text-white">ROSE GIRL PAINTING</h2><!-- / hover-title -->
+                                <p class="gallery-info text-center text-white">Framed - From $99.99
+                                    <span class="gallery-icons">
+                                        <a href="#x" class="gallery-button" data-toggle="modal" data-target=".framed-product"><i class="fas fa-plus"></i></a>
+                                        <a href="shopping-cart.html" class="gallery-button"><i class="fas fa-shopping-cart"></i></a>
+                                    </span><!--/ gallery-icons -->
+                                </p><!-- / gallery-info -->
+                            </div><!-- / hover-content -->
+                        </figcaption>
+                    </figure><!-- / gallery-item -->
+                </li><!-- / gallery -->
+
+                <!-- gallery -->
+                <li class="col-md-6 col-lg-4 gallery" data-groups='["framed"]'>
+                    <figure class="gallery-item effect-bubba">
+                        <img src="images/gallery4.jpg" alt="">
+                        <figcaption>
+                            <div class="hover-content">
+                                <h2 class="hover-title text-center text-white">COLORFULL PAINTING</h2><!-- / hover-title -->
+                                <p class="gallery-info text-center text-white">Framed - From $69.99
+                                    <span class="gallery-icons">
+                                        <a href="#x" class="gallery-button" data-toggle="modal" data-target=".framed-product"><i class="fas fa-plus"></i></a>
+                                        <a href="shopping-cart.html" class="gallery-button"><i class="fas fa-shopping-cart"></i></a>
+                                    </span><!--/ gallery-icons -->
+                                </p><!-- / gallery-info -->
+                            </div><!-- / hover-content -->
+                        </figcaption>
+                    </figure><!-- / gallery-item -->
+                </li><!-- / gallery -->
+
+                <!-- gallery -->
+                <li class="col-md-6 col-lg-4 gallery" data-groups='["photography", "print"]'>
+                    <figure class="gallery-item effect-bubba">
+                        <img src="images/gallery5.jpg" alt="">
+                        <figcaption>
+                            <div class="hover-content">
+                                <h2 class="hover-title text-center text-white">GALLERY PHOTGRAPHY</h2><!-- / hover-title -->
+                                <p class="gallery-info text-center text-white">Photography, Print - From $4.99
+                                    <span class="gallery-icons">
+                                        <a href="#x" class="gallery-button" data-toggle="modal" data-target=".print-product"><i class="fas fa-plus"></i></a>
+                                        <a href="shopping-cart.html" class="gallery-button"><i class="fas fa-shopping-cart"></i></a>
+                                    </span><!--/ gallery-icons -->
+                                </p><!-- / gallery-info -->
+                            </div><!-- / hover-content -->
+                        </figcaption>
+                    </figure><!-- / gallery-item -->
+                </li><!-- / gallery -->
+
+                <!-- gallery -->
+                <li class="col-md-6 col-lg-4 gallery tall-gallery" data-groups='["photography", "digital"]'>
+                    <figure class="gallery-item effect-bubba">
+                        <img src="images/mgallery6.jpg" alt="">
+                        <figcaption>
+                            <div class="hover-content">
+                                <h2 class="hover-title text-center text-white">SMOKE PHOTGRAPHY</h2><!-- / hover-title -->
+                                <p class="gallery-info text-center text-white">Photography, Framed - From $24.49
+                                    <span class="gallery-icons">
+                                        <a href="#x" class="gallery-button" data-toggle="modal" data-target=".framed-product"><i class="fas fa-plus"></i></a>
+                                        <a href="shopping-cart.html" class="gallery-button"><i class="fas fa-shopping-cart"></i></a>
+                                    </span><!--/ gallery-icons -->
+                                </p><!-- / gallery-info -->
+                            </div><!-- / hover-content -->
+                        </figcaption>
+                    </figure><!-- / gallery-item -->
+                </li><!-- / gallery -->
+
+                <!-- gallery -->
+                <li class="col-md-6 col-lg-4 gallery" data-groups='["photography", "digital"]'>
+                    <figure class="gallery-item effect-bubba">
+                        <img src="images/gallery7.jpg" alt="">
+                        <figcaption>
+                            <div class="hover-content">
+                                <h2 class="hover-title text-center text-white">PORTRAITS COLLECTION</h2><!-- / hover-title -->
+                                <p class="gallery-info text-center text-white">Photography, Digital - From $9.99
+                                    <span class="gallery-icons">
+                                        <a href="#x" class="gallery-button" data-toggle="modal" data-target=".digital-product"><i class="fas fa-plus"></i></a>
+                                        <a href="shopping-cart.html" class="gallery-button"><i class="fas fa-shopping-cart"></i></a>
+                                    </span><!--/ gallery-icons -->
+                                </p><!-- / gallery-info -->
+                            </div><!-- / hover-content -->
+                        </figcaption>
+                    </figure><!-- / gallery-item -->
+                </li><!-- / gallery -->
+
+                <!-- gallery -->
+                <li class="col-md-6 col-lg-4 gallery" data-groups='["photography", "print", "digital"]'>
+                    <figure class="gallery-item effect-bubba">
+                        <img src="images/gallery8.jpg" alt="">
+                        <figcaption>
+                            <div class="hover-content">
+                                <h2 class="hover-title text-center text-white">COLORFULL FLOWER</h2><!-- / hover-title -->
+                                <p class="gallery-info text-center text-white">Photography, Print, Digital - From $23.99
+                                    <span class="gallery-icons">
+                                        <a href="#x" class="gallery-button" data-toggle="modal" data-target=".digital-product"><i class="fas fa-plus"></i></a>
+                                        <a href="shopping-cart.html" class="gallery-button"><i class="fas fa-shopping-cart"></i></a>
+                                    </span><!--/ gallery-icons -->
+                                </p><!-- / gallery-info -->
+                            </div><!-- / hover-content -->
+                        </figcaption>
+                    </figure><!-- / gallery-item -->
+                </li><!-- / gallery -->
+
+                <!-- gallery -->
+                <li class="col-md-6 col-lg-4 gallery" data-groups='["framed"]'>
+                    <figure class="gallery-item effect-bubba">
+                        <img src="images/gallery9.jpg" alt="">
+                        <figcaption>
+                            <div class="hover-content">
+                                <h2 class="hover-title text-center text-white">REFLECTIONS</h2><!-- / hover-title -->
+                                <p class="gallery-info text-center text-white">Framed - From $74.99
+                                    <span class="gallery-icons">
+                                        <a href="#x" class="gallery-button" data-toggle="modal" data-target=".framed-product"><i class="fas fa-plus"></i></a>
+                                        <a href="shopping-cart.html" class="gallery-button"><i class="fas fa-shopping-cart"></i></a>
+                                    </span><!--/ gallery-icons -->
+                                </p><!-- / gallery-info -->
+                            </div><!-- / hover-content -->
+                        </figcaption>
+                    </figure><!-- / gallery-item -->
+                </li><!-- / gallery -->
+
+            </ul><!-- / gallery -->
+        </div><!-- / container -->
+    </section>
+    <!-- / gallery -->
 <div class="btn-group btn-group-toggle" data-toggle="buttons">
 
   
